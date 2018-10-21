d2635836c3a314f3afbbfdd94947c5cdf611ac08
johannamelly <johannamelly@gmail.com>
2018-10-21 18:57:11 +0200

    taking too much time, raging like hell(on aime l'ironie ici)

--- README.md
+++ README.md
@@ -12,4 +12,30 @@ Montrer des statistiques sur le degré de rage des utilisateurs GitHub, présent
 - Obtenir une étoile à son repo s'il n'y a aucun indicateur de rage présent  
 - Obtenir une couronne si tous ses repos ont une étoile  
 - Avoir accès aux messages enragés des utilisateurs  
-- Faire apparaître un commit rageux random
+- Faire apparaître un commit rageux random  
+
+##Rage calculation
+###Par repo
+Calcul d'un pourcentage. Un repo full hate = 100%  
+1/3 de pourcentage calculé selon vulgarité des commits  
+1/3 selon les deletions élevées  
+1/3 selon un potentiel rebase  
+
+Commits:  
+Si 0.5% des commits du repo sont dirty => 0.25 des 33% alloués
+Si 1% => 0.5  
+Si 2% => 0.75  
+Si >2% => 1  
+(on pourrait vraiment calculer le pourcentage de dirty commits et prendre ce pourcentage sur 33% mais les chiffres seraient bien trop bas)  
+  
+Deletions:  
+Si le nombre de deletions concernant un repo représente 1/4 des additions => 0.25 des 33% alloués  
+Si 1/3 => 0.5  
+Si 1/2 => 0.75  
+Si >1/2 => 1  
+  
+Si rebase: 1 des 33% alloués  
+  
+Moyenne des pourcentages obtenus dans tous les repos (ou juste les dirty?) = total hate  
+  
+Si repo = 0% hate => étoile
--- site/js/app.js
+++ site/js/app.js
@@ -1,74 +1,11 @@
-
 const baseUrl = "http://localhost:3000";
 let user;
-if(null != localStorage.getItem("user")){
-    document.getElementById("selectedUser").innerHTML = localStorage.getItem('user');
-}else{
-    document.getElementById("selectedUser").innerHTML = "octocat";
-}
+let integer = 65;
 
-
-function getUser(username){
-    
-    return fetch(`${baseUrl}/users/${username}`)
-    .then(response=>{
-        if (!response.ok){
-            throw new Error('Oups');
-        }
-        
-        return response.json();
-    })
-}
-
-function getCommits(username){
-    return fetch(`${baseUrl}/commits/${username}`)
-    .then(response=>{
-        if (!response.ok){
-            throw new Error('Oups');
-        }
-        
-        return response.json();
-    })
-}
-
-function getDirtyCommits(username){
-    return fetch(`${baseUrl}/dirtycommits/${username}`)
-    .then(response=>{
-        if (!response.ok){
-            throw new Error('Oups');
-        }
-        
-        return response.json();
-    })
-}
 
 function searchUser(){
     if( (null !=  document.getElementById("searchedUser").value) && ("" !=  document.getElementById("searchedUser").value)){
         localStorage.setItem("user", document.getElementById("searchedUser").value);
     document.location.href = "user.html";
     }
-    
 }
-
-getDirtyCommits(localStorage.getItem("user"))
-        .then(commits => {
-            const liste = commits;
-            for(let i = 0; i < commits.items.length; ++i){
-                 document.getElementById("a_commit").innerHTML += ("</br>" + i + ": " + liste.items[i].commit.message);
-            }
-        })
-
-
-
-getUser(localStorage.getItem("user"))
-.then(user=>{
-    const avatar = user;
-    avatar.src = user.avatar_url;
-    name.innerText = user.name;
-    console.log(avatar);
-    document.getElementById("user-avatar").src = avatar.avatar_url;
-    document.getElementById("selectedUser").innerHTML = avatar.login;
-
-})
-
-
